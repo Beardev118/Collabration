ea615e7f2149021d03426f5198fb1d3a395b17cd
microlion <microlion116>
2020-04-06 04:47:26 +1100

    butild the useeffect on the product page.

--- src/Components/SearchBar/SearchBarContext.js
+++ src/Components/SearchBar/SearchBarContext.js
@@ -8,8 +8,7 @@ export const SearchProvider = props=>{
   const init = new URLSearchParams();
 
     const [searchQuery,setSearchQuery] = useState(init);
-    const [searchData, setSearchData] = useState(null);
-    const [menu_Data, setMenuData] = useState(null);  
+   
 
 
     // const  GetBackenQuery = (searchParams)=>{
@@ -37,64 +36,7 @@ export const SearchProvider = props=>{
 
 
 
-    useEffect(() => {
-        let ignore = false;
-        let result = null;
-        console.log('This is search Quesry on useEffect')
-        console.log(searchQuery.toString());
-
-        console.log('This is the GetBackendQuery function');  
-        console.log(searchQuery);
-        console.log(searchQuery.toString());
-          var keys = ['search_q','country','category','size','brand'];
     
-    
-          var backendQuery = new URLSearchParams();
-    
-          for(var key of keys) { 
-            if (searchQuery.getAll(key).length===0) {
-              backendQuery.set(key,'null');
-            }else{
-              backendQuery.set(key,searchQuery.getAll(key).join('*'));
-            }
-          }
-          
-
-        
-        async function fetchProduct() { 
-          const response = await fetch('http://3.10.195.126:3000/products/search?'+backendQuery);
-          const json = await response.json();
-          const [item] = json.products;
-          const [categories] = json.category;
-          const [sizes] = json.size;
-          const [brands] = json.brand;
-          console.log(json.category);
-          [result] = json.results;
-          let menu_Data_fetch = null;
-          if (result==='ok') {
-            const categoryData = categories.map((label,index) => ({"key":'category'+index, "label":label,"selected":false,"menuKind":"category" }))
-            const sizeData =  sizes.map((label,index) => ({"key":'size'+index, "label":label,"selected":false,"menuKind":"size" }))
-            const brandData = brands.map((label,index) => ({"key":'brand'+index, "label":label,"selected":false,"menuKind":"brand" }));
-            menu_Data_fetch = categoryData.concat(sizeData,brandData);
-            console.log(menu_Data_fetch);
-            setMenuData({menu_Data:menu_Data_fetch});
-          }
-          setSearchData(
-            {
-              products:item,
-              result:result
-            }
-          )
-          if (!ignore) {
-        //   setProducts(item);
-            console.log('This is prduct Data')
-          }
-        }
-    
-        fetchProduct();
-        return () => { ignore = true };
-      }, [searchQuery]);
-
       console.log('****search query On Context');
           // console.log(searchData);  
           // console..products
@@ -104,7 +46,7 @@ export const SearchProvider = props=>{
 
     return(
         <SearchContext.Provider
-        value={{ searchData: [searchData, setSearchData],menu_Data:[menu_Data,setMenuData], searchQuery: [searchQuery, setSearchQuery]}}
+        value={{searchQuery: [searchQuery, setSearchQuery]}}
          >   
             {props.children}
         </SearchContext.Provider>
